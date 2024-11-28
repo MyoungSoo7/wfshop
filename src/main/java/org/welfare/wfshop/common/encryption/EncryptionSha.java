ce35d2596d6bfbea1c6f6f3a57aa9cd990243332
iamipro <iamipro@naver.com>
2024-11-28 19:04:44 +0900

    rf

--- src/main/java/org/welfare/wfshop/common/encryption/EncryptionSha.java
+++ src/main/java/org/welfare/wfshop/common/encryption/EncryptionSha.java
@@ -1,4 +1,29 @@
 package org.welfare.wfshop.common.encryption;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.nio.charset.StandardCharsets;
+
 public class EncryptionSha {
+    private static final String SHA_512 = "SHA-512";
+
+    public static String encryptString(String input) {
+        try {
+            MessageDigest digest = getInitializedDigest(input);
+            return formatDigest(digest);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static MessageDigest getInitializedDigest(String input) throws Exception {
+        MessageDigest digest = MessageDigest.getInstance(SHA_512);
+        digest.reset();
+        digest.update(input.getBytes(StandardCharsets.UTF_8));
+        return digest;
+    }
+
+    private static String formatDigest(MessageDigest digest) {
+        return String.format("%0128x", new BigInteger(1, digest.digest()));
+    }
 }
---
+++ src/main/java/org/welfare/wfshop/common/encryption/RSAUtil.java
@@ -0,0 +1,43 @@
+package org.welfare.wfshop.common.encryption;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.crypto.Cipher;
+import java.nio.charset.StandardCharsets;
+import java.security.PrivateKey;
+
+@Slf4j
+public class RSAUtil {
+
+    private final PrivateKey privateKey;
+
+    public RSAUtil(PrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public String decrypt(String securedValue) {
+        String decryptedText = "";
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+            byte[] encryptedBytes = hexToByteArray(securedValue);
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+            decryptedText = new String(decryptedBytes, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            log.error("Decrypt Exception Error: {}", e.getMessage());
+        }
+        return decryptedText;
+    }
+
+    public static byte[] hexToByteArray(String hex) {
+        if (hex == null || hex.length() % 2 != 0) {
+            return new byte[]{};
+        }
+        byte[] bytes = new byte[hex.length() / 2];
+        for (int i = 0; i < hex.length(); i += 2) {
+            byte value = (byte) Integer.parseInt(hex.substring(i, i + 2), 16);
+            bytes[i / 2] = value;
+        }
+        return bytes;
+    }
+}
